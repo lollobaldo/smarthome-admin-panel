3e70ca6be0cf9445908d072ce35a1eb4911d2546
Lorenzo <lollobaldo2000@gmail.com>
2021-01-10 15:46:55 +0100

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { MqttProvider } from 'brains/mqtt';
+import { useTheme } from 'brains/hooks';
 
 import Header from 'components/Header';
 // import Sidebar from './components/Sidebar';
@@ -11,12 +12,10 @@ import Content from 'components/Content';
 import { Theme, GlobalStyle } from 'styles/theme';
 
 const App = () => {
-  const [theme, setTheme] = useState<Theme>('light');
-
-  // const { mode, activePreset } = state;
+  const [theme, setTheme] = useTheme();
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    setTheme((theme === 'light') ? 'dark' : 'light');
   };
 
   return (
--- src/brains/hooks.ts
+++ src/brains/hooks.ts
@@ -1,4 +1,6 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
+
+import { Theme } from 'styles/theme';
 
 export const useOneOf = <T>(selected: T | undefined = undefined) => {
   const [state, setState] = useState(selected);
@@ -8,4 +10,37 @@ export const useOneOf = <T>(selected: T | undefined = undefined) => {
   return [state, useCallback(change, [])];
 };
 
-export const emptyHook = () => {};
+export const useMediaQuery = (query: string, defaultState = false) => {
+  const [state, setState] = useState(defaultState);
+
+  useEffect(() => {
+    let mounted = true;
+    const mql = window.matchMedia(query);
+    const onChange = () => {
+      if (!mounted) return;
+      setState(!!mql.matches);
+    };
+
+    mql.addListener(onChange);
+    setState(mql.matches);
+
+    return () => {
+      mounted = false;
+      mql.removeListener(onChange);
+    };
+  }, [query]);
+
+  return state;
+};
+
+export const useTheme = (defaultTheme: Theme = 'light'):
+[Theme, React.Dispatch<React.SetStateAction<Theme>>] => {
+  const [theme, setTheme] = useState(defaultTheme);
+  const userAgentTheme = useMediaQuery('(prefers-color-scheme: light)', defaultTheme === 'light');
+
+  useEffect(() => {
+    setTheme(userAgentTheme ? 'light' : 'dark');
+  }, [userAgentTheme]);
+
+  return [theme, setTheme];
+};
